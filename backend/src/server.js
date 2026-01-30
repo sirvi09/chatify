9a2406748a940bb35b7c8e807e8738254cbfc128
sirvi09 <solankilalit538@gmail.com>
2026-01-31 00:03:35 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -23,5 +26,6 @@ app.get("/", (_,res) => {
     app.use(express.static(path.join(__dirname,"../frontend","dist","index.html")))
 })
 }
-app.listen(PORT, () => console.log("server is running on port: " + PORT));
-
+ app.listen(PORT, () => {
+    console.log("server is running on port: " + PORT)
+ });
