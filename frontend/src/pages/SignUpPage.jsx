e7f108b09f41edd7b6c3f45caf042a6716749c52
sirvi09 <solankilalit538@gmail.com>
2026-02-02 23:11:43 +0530

    frontend setup added

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignUpPage() {
+    const {authUser,isLoading, login } = useAuthStore()
+  return (
+    
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignUpPage
